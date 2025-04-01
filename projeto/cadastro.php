3cb5215aa6fe472349014af8db8d67aad0be9c2e
KauaXavier1 <kebxmine@gmail.com>
2025-03-31 21:56:21 -0300

    Update cadastro.php

--- projeto/cadastro.php
+++ projeto/cadastro.php
@@ -4,4 +4,30 @@ include('functions.php');
 
 // cadastrar livro
 
-if ($_SERVER['RESQUEST_METHOD']) ===
+if ($_SERVER['RESQUEST_METHOD']) === 'POST' && isset($_POST['cadastrar_livro'])) {
+    verificarLogin('biblio');
+    
+    $titulo = $_POST['titulo'];
+    $autor = $_POST['autor'];
+    $editora = $_POST['editora'];
+    $isbn = $_POST['isbn'];
+    
+    $linha = "$titulo|$autor|$editora|$isbn" . PHP_EOL;
+    file_put_contents('livros.txt', $linha, FILE_APPEND);
+    echo "Livro cadastrado com sucesso!";
+}
+
+// Cadastrar pedido
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cadastrar_pedido'])) {
+    verificarLogin('professor');
+    
+    $titulo = $_POST['titulo'];
+    $autor = $_POST['autor'];
+    $editora = $_POST['editora'];
+    $isbn = $_POST['isbn'];
+    
+    $linha = "$titulo|$autor|$editora|$isbn" . PHP_EOL;
+    file_put_contents('pedidos.txt', $linha, FILE_APPEND);
+    echo "Pedido cadastrado com sucesso!";
+}
+?>
