8f74f86a35679ebaf060a41252449dca7a263fa8
KauaXavier1 <kebxmine@gmail.com>
2025-03-31 21:52:59 -0300

    Update cadastro.php

--- projeto/cadastro.php
+++ projeto/cadastro.php
@@ -1,10 +1,24 @@
 <?php
 session_start();
 
-// Verificação de login
+// verificação de login
 function verificarLogin($tipo) {
     if (!isset($_SESSION['tipo']) || $_SESSION['tipo'] !== $tipo) {
         header("Location: login.php");
         exit();
     }
+}
+// cadastro de livro
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cadastrar_livro'])) {
+    verificarLogin('biblio');  // Apenas bibliotecário pode cadastrar livros
+    
+    $titulo = $_POST['titulo'];
+    $autor = $_POST['autor'];
+    $editora = $_POST['editora'];
+    $isbn = $_POST['isbn'];
+
+    // verificação de existência do arquivo e cadastro do livro
+    $livro = "$titulo|$autor|$editora|$isbn" . PHP_EOL;
+    file_put_contents('livros.txt', $livro, FILE_APPEND);
+    echo "Livro cadastrado com sucesso!";
 }
